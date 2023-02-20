c8bd93204333069639d9b4695216f0e3109ceec1
Joern Weissenborn <joern.weissenborn@gmail.com>
2023-02-20 20:25:30 +0100

    Added config for elenia.

--- hosts/default.nix
+++ hosts/default.nix
@@ -11,6 +11,35 @@ let
   lib = nixpkgs.lib;
 in
 {
+  elenia = lib.nixosSystem {
+    # Laptop
+    inherit system;
+    specialArgs = {
+      inherit inputs user location;
+      host = {
+        hostName = "elenia";
+      };
+    };
+    modules = [
+      ./elenia
+      ./configuration.nix
+
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {
+          inherit user;
+          host = {
+            hostName = "elenia";
+          };
+        };
+        home-manager.users.${user} = {
+          imports = [ (import ./home.nix) ];
+        };
+      }
+    ];
+  };
   vbox = lib.nixosSystem {
     # VM profile
     inherit system;
