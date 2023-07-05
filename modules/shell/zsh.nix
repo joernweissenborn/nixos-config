8191dcb92f9644ab9a167b54095596ee66642bc2
Joern Weissenborn <joern.weissenborn@gmail.com>
2023-07-06 00:33:04 +0200

    Update zsh conf

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -5,7 +5,7 @@
     zsh = {
       enable = true;
       enableAutosuggestions = true;
-      enableSyntaxHighlighting = true;
+      syntaxHighlighting.enable = true;
       enableCompletion = true;
       history.size = 100000;
 
